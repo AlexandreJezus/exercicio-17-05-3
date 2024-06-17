480fa348886541965b42f49c6cff7e22ac80e9b3
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-06-17 11:44:24 -0300

    feat

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-let array = [1, 2, 2, 3, 3, 4, 5, 6];
-let comSpread = [...array, array];
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log(comSpread);
+let todos = [];
+
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] % 2 == 0) {
+    todos.unshift(numeros[i]);
+  } else {
+    todos.push(numeros[i]);
+  }
+}
+
+console.log(todos);
